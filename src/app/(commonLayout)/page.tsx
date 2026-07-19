5664aa17eb6a3109ce2edb61e58006d81e5b8ab0
muntasir010 <naeemtasir03@gmail.com>
2026-07-19 22:42:38 +0600

    feat(specialties): implement simple specialties design

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/modules/Home/Hero";
+import Specialties from "@/components/modules/Home/Specialties";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Specialties/>
     </>
   );
 }
