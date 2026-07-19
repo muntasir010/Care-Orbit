eaf449891a0cd5fb2c333290d34a2e96b5092f94
muntasir010 <naeemtasir03@gmail.com>
2026-07-19 23:32:28 +0600

    feat(top rated doctors): implement top rated doctors design

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/modules/Home/Hero";
 import Specialties from "@/components/modules/Home/Specialties";
+import TopRatedDoctors from "@/components/modules/Home/TopRatedDoctor";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Specialties/>
+      <TopRatedDoctors/>
     </>
   );
 }
