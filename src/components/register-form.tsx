bad3adb1ccb85e50506349deff84147d79ba61dc
muntasir010 <naeemtasir03@gmail.com>
2026-07-21 16:11:44 +0600

    feat(auth): patient created successfully

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { useActionState } from "react";
 import { Button } from "./ui/button";
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
+import { registerPatient } from "@/services/auth/registerPatient";
 
 const RegisterForm = () => {
+  const [state, formAction, isPending] = useActionState(registerPatient, null);
+  console.log(state);
   return (
-    <form >
+    <form action={formAction}>
       <FieldGroup>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {/* Name */}
@@ -51,8 +55,8 @@ const RegisterForm = () => {
         </div>
         <FieldGroup className="mt-4">
           <Field>
-            <Button type="submit">
-                Register
+            <Button type="submit" disabled={isPending}>
+              {isPending ? "Creating Account..." : "Create Account"}
             </Button>
 
             <FieldDescription className="px-6 text-center">
