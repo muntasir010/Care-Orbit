b6e90421c70bb7d0f76b61873245aa99f7757247
muntasir010 <naeemtasir03@gmail.com>
2026-08-02 00:40:33 +0600

    feat(auth): logged out success toast implement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import LoginSuccessToast from "@/components/shared/LoginSuccessToast";
+import LogoutSuccessToast from "@/components/shared/LogoutSuccessToast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         {children}
         <Toaster position="top-right" richColors />
         <LoginSuccessToast />
+        <LogoutSuccessToast />
       </body>
     </html>
   );
