4fecd773916bbc1bc1677752afe9aeaf0cfabec0
muntasir010 <naeemtasir03@gmail.com>
2026-08-09 20:16:02 +0600

    feat(dashboard): create reuseable management page header component

--- src/components/shared/ManagementTable.tsx
+++ src/components/shared/ManagementTable.tsx
@@ -34,10 +34,6 @@ interface ManagementTableProps<T> {
   isRefreshing?: boolean;
 }
 
-// const ManagementTable<T> = (props: ManagementTableProps<T>) => {
-//   return <div>ManagementTable</div>;
-// };
-
 function ManagementTable<T>({
   data = [],
   columns = [],
