a87419e577fa8af54fde26495337081e932d37ff
muntasir010 <naeemtasir03@gmail.com>
2026-08-15 20:19:13 +0600

    feat(admin): create specialties mangaement page header

--- src/app/(dashboardLayout)/admin/dashboard/specialties-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialties-management/page.tsx
@@ -1,17 +1,15 @@
+import SpecialtiesManagementHeader from "@/components/modules/Admin/SpecialtiesManagement/SpecialtiesManagementHeader";
 import RefreshButton from "@/components/shared/RefreshButton";
-import { TableSkeleton } from "@/components/shared/TableSkeleton";
 import { getSpecialties } from "@/services/admin/specialtiesManagement";
-import { Suspense } from "react";
 
 const AdminSpecialtiesManagementPage = async () => {
-  const result = await getSpecialties();
+ const result = await getSpecialties();
   return (
     <div className="space-y-6">
+      <SpecialtiesManagementHeader />
       <div className="flex">
         <RefreshButton />
       </div>
-      <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
-      </Suspense>
     </div>
   );
 };
