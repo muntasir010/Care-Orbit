20766b59884ffd5eeba54264aae535f6afbe175d
muntasir010 <naeemtasir03@gmail.com>
2026-08-20 21:36:57 +0600

    feat(doctors management): doctor created by admin successfully

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,14 +1,22 @@
+
 import DoctorsManagementHeader from "@/components/modules/Admin/DoctorsManagement.tsx/DoctorsManagementHeader";
+import DoctorsTable from "@/components/modules/Admin/DoctorsManagement.tsx/DoctorsTable";
 import RefreshButton from "@/components/shared/RefreshButton";
 import SearchFilter from "@/components/shared/SearchFilter";
 import SelectFilter from "@/components/shared/SelectFilter";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { getDoctors } from "@/services/admin/doctorManagement";
 import { getSpecialties } from "@/services/admin/specialtiesManagement";
-import { ISpecialty } from "@/types/specialties.interface";
+import { ISpecialty } from "@/types/specialties.interface"
+import { Suspense } from "react";
 
-const AdminDoctorsManagementPage = async ({}: {
+const AdminDoctorsManagementPage = async ({
+  searchParams,
+}: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) => {
   const specialtiesResult = await getSpecialties();
+  const doctorsResult = await getDoctors();
   return (
     <div className="space-y-6">
       <DoctorsManagementHeader specialties={specialtiesResult.data} />
@@ -16,7 +24,7 @@ const AdminDoctorsManagementPage = async ({}: {
         <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
         <SelectFilter
           paramName="specialty" // ?specialty="Cardiology"
-          options={specialtiesResult.data.map((specialty: ISpecialty) => ({
+          options={specialtiesResult?.data.map((specialty: ISpecialty) => ({
             label: specialty.title,
             value: specialty.title,
           }))}
@@ -24,6 +32,16 @@ const AdminDoctorsManagementPage = async ({}: {
         />
         <RefreshButton />
       </div>
+      <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
+        <DoctorsTable
+          doctors={doctorsResult.data}
+          specialties={specialtiesResult.data}
+        />
+        {/* <TablePagination
+          currentPage={doctorsResult.meta.page}
+          totalPages={totalPages}
+        /> */}
+      </Suspense>
     </div>
   );
 };
