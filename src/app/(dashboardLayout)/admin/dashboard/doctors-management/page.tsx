867e32871f59035f2352cbe57dce65e25a257bf6
muntasir010 <naeemtasir03@gmail.com>
2026-08-20 23:48:59 +0600

    feat(doctors management): implement pagination on doctors management table

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,14 +1,14 @@
-
 import DoctorsManagementHeader from "@/components/modules/Admin/DoctorsManagement.tsx/DoctorsManagementHeader";
 import DoctorsTable from "@/components/modules/Admin/DoctorsManagement.tsx/DoctorsTable";
 import RefreshButton from "@/components/shared/RefreshButton";
 import SearchFilter from "@/components/shared/SearchFilter";
 import SelectFilter from "@/components/shared/SelectFilter";
+import TablePagination from "@/components/shared/TablePagination";
 import { TableSkeleton } from "@/components/shared/TableSkeleton";
 import { queryStringFormatter } from "@/lib/formatters";
 import { getDoctors } from "@/services/admin/doctorManagement";
 import { getSpecialties } from "@/services/admin/specialtiesManagement";
-import { ISpecialty } from "@/types/specialties.interface"
+import { ISpecialty } from "@/types/specialties.interface";
 import { Suspense } from "react";
 
 const AdminDoctorsManagementPage = async ({
@@ -20,6 +20,10 @@ const AdminDoctorsManagementPage = async ({
   const queryString = queryStringFormatter(searchParamsObj);
   const specialtiesResult = await getSpecialties();
   const doctorsResult = await getDoctors(queryString);
+
+  const totalPages = Math.ceil(
+    doctorsResult.meta.total / doctorsResult.meta.limit,
+  );
 
   return (
     <div className="space-y-6">
@@ -41,10 +45,10 @@ const AdminDoctorsManagementPage = async ({
           doctors={doctorsResult.data}
           specialties={specialtiesResult.data}
         />
-        {/* <TablePagination
+        <TablePagination
           currentPage={doctorsResult.meta.page}
           totalPages={totalPages}
-        /> */}
+        />
       </Suspense>
     </div>
   );
