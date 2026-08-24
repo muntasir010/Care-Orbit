5fd0e058ef91d1bed8d91ead1d6c353a850ff4fa
muntasir010 <naeemtasir03@gmail.com>
2026-08-24 20:59:01 +0600

    feat(patients management): create getPatientById for patients management

--- src/services/admin/patientsManagement.ts
+++ src/services/admin/patientsManagement.ts
@@ -28,3 +28,22 @@ export async function getPatients(queryString?: string) {
         };
     }
 }
+
+export async function getPatientById(id: string) {
+    try {
+        const response = await serverFetch.get(`/patient/${id}`, {
+            next: {
+                tags: [`patient-${id}`, "patients-list"],
+                revalidate: 180, // more responsive patient profile updates
+            }
+        })
+        const result = await response.json();
+        return result;
+    } catch (error: any) {
+        console.log(error);
+        return {
+            success: false,
+            message: `${process.env.NODE_ENV === 'development' ? error.message : 'Something went wrong'}`
+        };
+    }
+}
