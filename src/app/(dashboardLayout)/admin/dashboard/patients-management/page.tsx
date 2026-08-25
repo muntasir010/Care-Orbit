d4584a79e5e1ffb448de7b2dfbff03942b7497a0
muntasir010 <naeemtasir03@gmail.com>
2026-08-25 20:25:11 +0600

    feat(patients management): create patientsColumns for patients management

---
+++ src/app/(dashboardLayout)/admin/dashboard/patients-management/page.tsx
@@ -0,0 +1,14 @@
+import ManagementPageHeader from "@/components/shared/ManagementPageHeader"
+
+const PatientsManagementPage = () => {
+  return (
+     <div className="space-y-6">
+      <ManagementPageHeader
+        title="Patients Management"
+        description="Manage patients information and details"
+      />
+    </div>
+  )
+}
+
+export default PatientsManagementPage
