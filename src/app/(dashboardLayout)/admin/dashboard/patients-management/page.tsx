3cb48ef3ffc53b0cc58c2c485bb9241fb65d94e3
muntasir010 <naeemtasir03@gmail.com>
2026-08-25 22:22:15 +0600

    feat(patients management): create patients table for patients management

--- src/app/(dashboardLayout)/admin/dashboard/patients-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/patients-management/page.tsx
@@ -1,12 +1,22 @@
+import PatientsTable from "@/components/modules/Admin/PatientsManagement/PatientsTable"
 import ManagementPageHeader from "@/components/shared/ManagementPageHeader"
+import { TableSkeleton } from "@/components/shared/TableSkeleton"
+import { getPatients } from "@/services/admin/patientsManagement"
+import { Suspense } from "react"
 
-const PatientsManagementPage = () => {
+const PatientsManagementPage = async() => {
+
+    const patientsResult = await getPatients();
   return (
      <div className="space-y-6">
       <ManagementPageHeader
         title="Patients Management"
         description="Manage patients information and details"
       />
+
+      <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
+        <PatientsTable patients={patientsResult?.data || []} />
+      </Suspense>
     </div>
   )
 }
