e90f42104e4f63fc9aaa2344713acaf615992a23
muntasir010 <naeemtasir03@gmail.com>
2026-09-16 04:57:43 +0600

    feat(patient): create createAppointmentWihtPayLater server function

--- src/services/patient/appointments.service.ts
+++ src/services/patient/appointments.service.ts
@@ -34,3 +34,33 @@ export async function createAppointment(data: IAppointmentFormData) {
         };
     }
 }
+
+export async function createAppointmentWithPayLater(data: IAppointmentFormData) {
+    try {
+        const response = await serverFetch.post("/appointment/pay-later", {
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const result = await response.json();
+        if (result.success) {
+            revalidateTag('my-appointments', { expire: 0 });
+            revalidateTag('appointments-list', { expire: 0 });
+            revalidateTag('patient-dashboard-meta', { expire: 0 });
+            revalidateTag('admin-dashboard-meta', { expire: 0 });
+            revalidateTag('doctor-dashboard-meta', { expire: 0 });
+        }
+        return result;
+    } catch (error: any) {
+        console.error("Error creating appointment with pay later:", error);
+        return {
+            success: false,
+            message:
+                process.env.NODE_ENV === "development"
+                    ? error.message
+                    : "Failed to book appointment",
+        };
+    }
+}
